7d5f8bbdddb15f20240cdc1829f56da4f3a25434
Yeon2003 <148292350+Yeon2003@users.noreply.github.com>
2023-11-21 18:02:29 -0500

    Add files via upload

---
+++ Yeonjoo_c1if23/entries/entry10/main.js
@@ -0,0 +1,90 @@
+function myFunction1() {
+  document.getElementById("reply").innerHTML = "go to question no.2";
+}
+
+function myFunction2() {
+  document.getElementById("reply").innerHTML = "...";
+}
+
+function myFunction3() {
+  document.getElementById("reply").innerHTML = "...";
+}
+
+function myFunction4() {
+  document.getElementById("reply").innerHTML = ".....";
+}
+function myFunction5() {
+  document.getElementById("reply2").innerHTML = "Good. May your life be a side walk";
+}
+
+function myFunction6() {
+  document.getElementById("reply2").innerHTML = "go to question no.3";
+}
+
+function myFunction7() {
+  document.getElementById("reply2").innerHTML = "Good. May your life be a side walk";
+}
+
+function myFunction8() {
+  document.getElementById("reply2").innerHTML = "Good. May your life be a side walk";
+}
+function myFunction9() {
+  document.getElementById("reply3").innerHTML = "Really?";
+}
+
+function myFunction10() {
+  document.getElementById("reply3").innerHTML = "Really?";
+}
+
+function myFunction11() {
+  document.getElementById("reply3").innerHTML = "Really?";
+}
+
+function myFunction12() {
+  document.getElementById("reply3").innerHTML = "go to question no.4";
+}
+function myFunction13() {
+  document.getElementById("reply4").innerHTML = "Wrong book!";
+}
+
+function myFunction14() {
+  document.getElementById("reply4").innerHTML = "go to question no.5";
+}
+
+function myFunction15() {
+  document.getElementById("reply4").innerHTML = "¯\\_(ツ)_/¯";
+}
+
+function myFunction16() {
+  document.getElementById("reply4").innerHTML = "¯\\_(ツ)_/¯";
+}
+function myFunction17() {
+  document.getElementById("reply5").innerHTML = "Close!";
+}
+
+function myFunction18() {
+  document.getElementById("reply5").innerHTML = "Think again";
+}
+
+function myFunction19() {
+  document.getElementById("reply5").innerHTML = "Think again";
+}
+
+function myFunction20() {
+  document.getElementById("reply5").innerHTML = "go to question no.6";
+}
+function myFunction21() {
+  document.getElementById("reply6").innerHTML = "go to question no.7";
+}
+
+function myFunction22() {
+  document.getElementById("reply6").innerHTML = "Read the book again";
+}
+function myFunction23() {
+  document.getElementById("reply7").innerHTML = "Does the method include more than one of the five seeds that the author mentions?";
+}
+
+function myFunction24() {
+  document.getElementById("reply7").innerHTML = "Take your time";
+}
+
